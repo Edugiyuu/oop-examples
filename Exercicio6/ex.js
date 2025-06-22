70a310f9560bba0c8426dbc1ba09527691ab1f5f
Eduardo Santos <edupaz077@gmail.com>
2025-06-22 03:20:50 -0300

    feat: one more ex done

--- Exercicio6/ex.js
+++ Exercicio6/ex.js
@@ -16,3 +16,45 @@ Atributo: produtos (array de produtos)
 Método: listarProdutos() → imprime todos os produtos com nome e estoque
 
 Método: venderProduto(nome, quantidade) → procura o produto pelo nome e chama vender */
+
+class Produto {
+    constructor(nome, preco, estoque) {
+        this.nome = nome
+        this.preco = preco
+        this.estoque = estoque
+    }
+    vender(quantidade){
+        if (this.estoque >= quantidade) {
+            this.estoque -= quantidade
+        }else{
+            console.log("Estoque insuficiente");
+        }
+    }
+}
+class Loja {
+    constructor(produtos) {
+        this.produtos = produtos
+    }
+    listarProdutos(){
+        for (let i = 0; i < this.produtos.length; i++) {
+            console.log(this.produtos[i]);
+        }
+    }
+    venderProduto(nome, quantidade){
+        for (let i = 0; i < this.produtos.length; i++) {
+            if (this.produtos[i].nome === nome) {
+                this.produtos[i].vender(quantidade);
+                console.log(`Vendido ${quantidade} unidades de ${this.produtos[i].nome}, estoque restante: ${this.produtos[i].estoque}`);
+            }
+        }
+    }
+}
+
+var produtoMiojo = new Produto("miojo", 6.90, 60)
+var produtoSuco = new Produto("suco", 4.00, 90)
+produtoMiojo.vender(3)
+
+var lojaDoZé = new Loja ([produtoMiojo,produtoSuco])
+
+lojaDoZé.listarProdutos()
+lojaDoZé.venderProduto("suco", 20)
