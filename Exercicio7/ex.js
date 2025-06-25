fdb7e7b1621e4f6157079b39034babd175a3ad90
Eduardo Santos <edupaz077@gmail.com>
2025-06-25 11:35:01 -0300

    ex7 done

--- Exercicio7/ex.js
+++ Exercicio7/ex.js
@@ -10,3 +10,69 @@ Método: emprestar() → só empresta se disponivel === true
 Método: devolver() → define disponivel = true
 
 ⚠️ Use get e set, e atributos privados com # */
+
+class Livro {
+  #titulo;
+  #autor;
+  #disponivel;
+
+  constructor(titulo, autor, disponivel) {
+    this.#titulo = titulo;
+    this.#autor = autor;
+    this.#disponivel = disponivel;
+  }
+
+  // GETTERS
+  get titulo() {
+    return this.#titulo;
+  }
+
+  get autor() {
+    return this.#autor;
+  }
+
+  get disponivel() {
+    return this.#disponivel;
+  }
+
+  set titulo(novoTitulo) {
+    this.#titulo = novoTitulo;
+  }
+
+  set autor(novoAutor) {
+    this.#autor = novoAutor;
+  }
+
+  set disponivel(novaDisponibilidade) {
+    this.#disponivel = novaDisponibilidade;
+  }
+
+  emprestar() {
+    if (this.#disponivel) {
+      console.log(`"${this.#titulo}" emprestado com sucesso`);
+      this.#disponivel = false;
+    } else {
+      console.log(`"${this.#titulo}" não está disponível para empréstimo`);
+    }
+  }
+
+  devolver() {
+    if (!this.#disponivel) {
+      console.log(`"${this.#titulo}" foi devolvido`);
+      this.#disponivel = true;
+    } else {
+      console.log(`"${this.#titulo}" já está disponível`);
+    }
+  }
+}
+
+const harryPoter = new Livro("Harry Poter", "Pessoa do Herry", true);
+
+console.log(harryPoter.titulo);
+console.log(harryPoter.titulo = "Harry pooper");
+
+
+harryPoter.emprestar();
+harryPoter.emprestar();
+harryPoter.devolver();
+harryPoter.devolver();
